9c17405e9e23515914ef1b5cb22684b776c88588
edro360 <redwin925a@gmail.com>
2025-11-11 17:36:10 -0600

    Added live exam file.

---
+++ JavaScript/nov11/examen.js
@@ -0,0 +1,17 @@
+const fizzBuzz = (limit) => {
+  for (let i = 1; i <= limit; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("FizzBuzz", i);
+    } else {
+      if (i % 3 === 0) {
+        console.log("Fizz", i);
+      } else if (i % 5 === 0) {
+        console.log("Buzz", i);
+      } else {
+        console.log(i);
+      }
+    }
+  }
+}
+
+fizzBuzz(100);
